11b57b2c5bd8887aebd30daa159c7ea61dc0b2d4
mateohubert2 <mathub2003@gmail.com>
2024-07-28 15:22:37 +0200

    Retour a la page home depuis les deux services

---
+++ Frontend/polynetdefender.js
@@ -0,0 +1,9 @@
+function run() {
+    document.getElementById("home").addEventListener("click", () => {
+        document.getElementById("loader-home").style.display = "block";
+        setTimeout(function() {
+            window.location.href = "http://localhost:8080/home.html";
+        }, 2500);
+    });
+}
+window.addEventListener("load", run);
