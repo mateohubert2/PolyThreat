dd254bb344f5b67ace1fe6d595df263fa82aa3da
mateohubert2 <mathub2003@gmail.com>
2024-08-04 18:59:34 +0200

    Ajout de la méthode de connection sur toutes les pages

--- Frontend/polybreach.js
+++ Frontend/polybreach.js
@@ -1,9 +1,27 @@
 function run(){
-    document.getElementById("home").addEventListener("click", () => {
-        document.getElementById("loader-home").style.display = "block";
-        setTimeout(function() {
-            window.location.href = "http://localhost:8080/home.html";
-        }, 2500);
-    });
+    check().then(() => {
+        document.getElementById("home").addEventListener("click", () => {
+            document.getElementById("loader-home").style.display = "block";
+            setTimeout(function() {
+                window.location.href = "http://localhost:8080/home.html";
+            }, 2500);
+        });
+    })
 }
+
+async function check(){
+    const connectID = localStorage.getItem("connectID");
+    console.log(connectID);
+    const response = await fetch("http://localhost:8080/verifyconnectid", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ connectID })
+    });
+    if (response.status !== 200) {
+        window.location.href = "http://localhost:8080/login.html";
+    }
+}
+
 window.addEventListener("load", run);
